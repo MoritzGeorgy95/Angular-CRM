7d78402e10911a5f907956a747f4aca32b4c8158
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-04-20 16:47:41 +0200

    backup before code refactoring

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { UsersService } from '../users.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,19 +11,44 @@ import { Router } from '@angular/router';
 export class LoginComponent {
 
   hide:boolean= true;
-  guestEmail:string;
-  guestPassword:string;
+  email:string;
+  password:string;
+  signupMode: boolean = false;
+  signUpSuccess:boolean= false;
 
-  constructor(public router: Router) {
+  constructor(public router: Router, private auth: Auth, private usersService: UsersService) {
     
   }
 
   guestLogin() {
-    this.guestEmail= 'guest@email.com';
-    this.guestPassword= 'password'
+    this.email= 'guest@email.com';
+    this.password= 'password'
 
     setTimeout(()=> {
       this.router.navigateByUrl('/dashboard');
     }, 1500)
+  }
+
+  logIn() {
+    signInWithEmailAndPassword(this.auth, this.email, this.password).then(() => {
+      // this.router.navigateByUrl('/dashboard');
+      console.log(this.auth.currentUser?.uid)
+    })
+    .catch(error => {
+      alert(error)
+    });
+  }
+
+  signUp() {
+    createUserWithEmailAndPassword(this.auth, this.email, this.password).then(() => {
+      this.signUpSuccess= true;
+
+      setTimeout(()=> {
+
+      }, 1000)
+
+    }).catch(error => {
+      alert(error)
+    })
   }
 }
