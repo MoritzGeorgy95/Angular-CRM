055dea0568ae8bab8d635baf631ca002a5bde41b
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-05-15 12:32:29 +0200

    added documentation to the login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,71 +1,95 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from '@angular/fire/auth';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from '@angular/fire/auth';
 import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-
-  hide:boolean= true;
-  email:string;
-  password:string;
+  hide: boolean = true;
+  email: string;
+  password: string;
   userName: string;
   signupMode: boolean = false;
-  signUpSuccess:boolean= false;
+  signUpSuccess: boolean = false;
 
-  constructor(public router: Router, private auth: Auth, private usersService: UsersService) {
-    
-  }
+  /**
+   * @param router The router service used for navigation.
+   * @param auth The authentication service used for user authentication on firebase.
+   * @param usersService The UsersService used for database operations.
+   */
 
+  constructor(
+    public router: Router,
+    private auth: Auth,
+    private usersService: UsersService
+  ) {}
+
+  /**
+   * Performs a guest login by setting predefined email and password,
+   * connecting to the database, and navigating to the dashboard.
+   */
   async guestLogin() {
-    this.email= 'guest@email.com';
-    this.password= 'password'
+    this.email = 'guest@email.com';
+    this.password = 'password';
     this.usersService.connectToDatabase('guest', 'Guest');
     localStorage.setItem('authToken', 'guest');
-    
-    setTimeout(()=> { 
+
+    setTimeout(() => {
       this.router.navigateByUrl('/dashboard');
-    }, 1500)
+    }, 1500);
   }
 
+  /**
+   * Performs a login by using the provided email and password using firebase auth,
+   * connects to the database, and navigates to the dashboard upon successful login.
+   */
   async logIn() {
     try {
       await signInWithEmailAndPassword(this.auth, this.email, this.password);
       const user = this.auth.currentUser;
-      if(user) {
-        const authData= {id: user.uid, name: user.displayName};
+      if (user) {
+        const authData = { id: user.uid, name: user.displayName };
         localStorage.setItem('authToken', JSON.stringify(authData));
         this.usersService.connectToDatabase(user.uid, user.displayName || '');
         this.router.navigateByUrl('/dashboard');
       }
-     
+    } catch (error) {
+      alert(error);
     }
-    catch (error) {
-      alert(error)
-    }
-    
   }
 
+  /**
+   * Performs a signup by creating a new user with the provided email and password,
+   * updates the user's profile with the entered username, connects to the database,
+   * and navigates to the dashboard.
+   */
   async signUp() {
-     try {
-    const credential = await createUserWithEmailAndPassword(this.auth, this.email, this.password);
-    const user = credential.user;
-    if (user) {
-      this.signUpSuccess= true;
-      await updateProfile(user, { displayName: this.userName });
-      setTimeout(()=> {
-        this.usersService.addNotesDocument(this.auth.currentUser?.uid || '')
-        this.logIn()
-       
-      })
+    try {
+      const credential = await createUserWithEmailAndPassword(
+        this.auth,
+        this.email,
+        this.password
+      );
+      const user = credential.user;
+      if (user) {
+        this.signUpSuccess = true;
+        await updateProfile(user, { displayName: this.userName });
+        setTimeout(() => {
+          this.usersService.addNotesDocument(this.auth.currentUser?.uid || '');
+          this.logIn();
+        });
+      }
+    } catch (error) {
+      alert(error);
     }
-  } catch (error) {
-    alert(error);
   }
-  }
-
 }
