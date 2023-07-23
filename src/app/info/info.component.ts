0518fcc7288ebe3f2d8279e76e075ce6653340ef
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-07-23 18:45:48 +0200

    updated font size of widgets

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogWalkthroughComponent } from '../dialog-walkthrough/dialog-walkthrough.component';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.scss']
+  styleUrls: ['./info.component.scss'],
 })
 export class InfoComponent {
+  constructor(private dialog: MatDialog) {}
 
+  openDialog() {
+    this.dialog.open(DialogWalkthroughComponent);
+  }
 }
